ae95adf6b0899698a36a09ff072088cbf62b403c
GreenestBeen <ameyers@alumni.cmu.edu>
2025-10-12 16:03:34 -0700

    Actually why isn't this just the same as standalone

--- archipelago/Logic.py
+++ archipelago/Logic.py
@@ -1299,7 +1299,7 @@ class LogicVarHolder:
 
     def CanFreeLanky(self):
         """Check if kong at Lanky location can be freed."""
-        return (self.spoiler.LocationList[Locations.LankyKong].item == Items.NoItem or self.hasMoveSwitchsanity(Switches.AztecLlamaPuzzle)) and self.swim
+        return (self.swim and self.hasMoveSwitchsanity(Switches.AztecLlamaPuzzle)) or self.CanPhase() or self.CanPhaseswim()
 
     def CanFreeChunky(self):
         """Check if kong at Chunky location can be freed."""
