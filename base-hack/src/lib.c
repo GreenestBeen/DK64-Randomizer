76d55b53b5305b78be0d95a1834118690f54597c
Ballaam <theballaam96@gmail.com>
2026-03-08 13:08:02 -0500

    Cannons as a move barebones

--- base-hack/src/lib.c
+++ base-hack/src/lib.c
@@ -1474,6 +1474,9 @@ int getTotalMoveCount(void) {
 	if (hasFlagMove(FLAG_ABILITY_CLIMBING)) {
 		count++;
 	}
+	if (checkFlag(FLAG_ABILITY_CANNON, FLAGTYPE_PERMANENT)) {
+		count++;
+	}
 	return count;
 }
 
